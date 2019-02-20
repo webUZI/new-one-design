d75f4168ce920d590a30fa8e44846b060dc6e6b8
laixiangran <laixiangran@baidu.com>
2019-02-20 16:43:02 +0800

    添加自动化脚本

---
+++ gitbook.sh
@@ -0,0 +1,11 @@
+git checkout master
+gitbook build
+git add .
+git commit -m $1
+git push -u origin master
+git checkout gh-pages
+cp -r _book/* .
+git add .
+git commit -m $1
+git push -u origin gh-pages
+git checkout master
